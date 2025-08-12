3c45bb372345c04a479995b6d9c0122773361b6c
D4ni11 <d4ni11c.a@gmail.com>
2025-08-12 16:39:19 -0300

    Solucion al problema de doble click usando stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+document.getElementById("miBoton").addEventListener("click", function(event) {
+    event.stopPropagation(); // Evita que el evento suba al div
+    alert("Hola!");
+});
+
 document.getElementById("contenedor").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
